5bd48393fc2b58f71c961ad45ac35d36fb08894e
Jake Hillion <jakehillion@meta.com>
2025-02-20 13:43:02 +0000

    nix: pin kernel revisions

--- .github/workflows/flake.nix
+++ .github/workflows/flake.nix
@@ -4,11 +4,25 @@
   inputs = {
     nixpkgs.url = "github:JakeHillion/nixpkgs/virtme-ng";
     flake-utils.url = "github:numtide/flake-utils";
+
+    "kernel_sched_ext_for-next".url = "git+https://git.kernel.org/pub/scm/linux/kernel/git/tj/sched_ext.git?ref=for-next";
+    "kernel_bpf-next".url = "git+https://git.kernel.org/pub/scm/linux/kernel/git/bpf/bpf-next.git";
+    "kernel_linux-rolling-stable".url = "git+https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git?ref=linux-rolling-stable";
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ]
       (system: {
+        packages = {
+          kernels = let
+            mkKernel = inp: {
+              
+            };
+          in {
+            "sched_ext/for-next" = mkKernel "sched_ext/for-next";
+          };
+        };
+
         devShells =
           let
             pkgs = import nixpkgs { inherit system; };
